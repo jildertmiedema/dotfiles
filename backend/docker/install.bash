43a89b45be1ac2ab923aec8f619017df1fd592a1
Jildert Miedema <git@jildertmiedema.nl>
2019-09-30 23:55:25 +0200

    release

--- backend/docker/install.bash
+++ backend/docker/install.bash
@@ -6,8 +6,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
-#RELEASE=`lsb_release -cs`
-RELEASE="xenial"
+RELEASE=`lsb_release -cs`
+#RELEASE="xenial"
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${RELEASE} stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
